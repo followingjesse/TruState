4d310029a2fb8b4d31dd1314b4b2da275bbb1879
Ali Abiba <abibaalih@gmail.com>
2018-04-30 12:39:56 -0500

    add module stuff

--- tru-state/src/app/app.module.ts
+++ tru-state/src/app/app.module.ts
@@ -7,17 +7,22 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {LoginComponent} from "./auth/login.component";
 import {AuthService} from "./auth/auth.service";
-import {Routes} from "@angular/router";
+import {HomeComponent} from "./home/home";
+import { RouterModule, Routes } from '@angular/router';
+import {UserCreateComponent} from "./users/user.create.component";
 
 const appRoutes: Routes = [
-  {path:'login', component:LoginComponent}
-]
+  {path:'login', component:LoginComponent},
+  {path:'create-user', component:UserCreateComponent}
+  //{path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent, LoginComponent
+    AppComponent, LoginComponent, HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
     BrowserModule, MatProgressSpinnerModule, HttpClientModule, MatInputModule
   ],
   providers: [AuthService],
