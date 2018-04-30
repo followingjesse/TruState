82049a7dccb9a2b5b5fba9d78e9aee8b47086f0b
Ali Abiba <abibaalih@gmail.com>
2018-04-30 17:34:34 -0500

    Fix routing pt.2

--- tru-state/src/app/app.module.ts
+++ tru-state/src/app/app.module.ts
@@ -7,18 +7,19 @@ import {LoginComponent} from "./auth/login.component";
 import {AuthService} from "./auth/auth.service";
 import { RouterModule, Routes } from '@angular/router';
 import {UserCreateComponent} from "./users/user.create.component";
+import {HomeComponent} from "./home/home.component";
 
 const appRoutes: Routes = [
-  {path:'home', component:AppComponent},
+  {path:'home', component:HomeComponent},
   {path:'login', component:LoginComponent},
   {path:'create-user', component:UserCreateComponent},
-  //{path:'', redirectTo:'/home', pathMatch: 'full'}
+  {path:'', redirectTo:'/home', pathMatch: 'full'}
   //{path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
   declarations: [
-    AppComponent, LoginComponent, UserCreateComponent
+    AppComponent, LoginComponent, UserCreateComponent, HomeComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
