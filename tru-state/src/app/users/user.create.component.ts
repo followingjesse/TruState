a1a43e82b478040a5839e7ae0eb12fdc8fd2683b
Ali Abiba <abibaalih@gmail.com>
2018-04-30 12:30:47 -0400

    Add pages

---
+++ tru-state/src/app/users/user.create.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from "@angular/core";
+
+Component({
+  selector: 'app-user-create-component',
+  templateUrl: 'login.component.html'
+})
+export class UserCreateComponent implements OnInit {
+  ngOnInit(){
+  }
+}
